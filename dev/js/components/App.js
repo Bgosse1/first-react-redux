ad01530468fb51e3bc2d2f8c406f49fb8d36f206
bgosse1 <bryant.gossett@wesuite.com>
2018-02-07 13:59:27 -0500

    use props

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import UserList from '../containers/user-list';
 require('../../scss/style.scss');
+
 
 const App = () => (
     <div>
         <h2>
             Username List: test
         </h2>   
+        <UserList/>
         <hr/>
         <h2>
             User Details:
